91b0b503beadc89edf18c49ae2aba15e7b33c7f4
talejalilov <talejalilov@gmail.com>
2022-08-27 14:49:55 +0300

    start CI CD

--- app/src/main/java/com/talejalilov/yukatechexercise/ExerciseApp.kt
+++ app/src/main/java/com/talejalilov/yukatechexercise/ExerciseApp.kt
@@ -1,6 +1,9 @@
 package com.talejalilov.yukatechexercise
 
 import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class ExerciseApp:Application() {
 }

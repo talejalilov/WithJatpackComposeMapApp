d3368db9dbc34a6f7ca5631886b5559e7d3487ac
talejalilov <talejalilov@gmail.com>
2022-08-30 19:44:10 +0300

    Map permission and background are ready

--- app/src/main/java/com/talejalilov/yukatechexercise/MainActivity.kt
+++ app/src/main/java/com/talejalilov/yukatechexercise/MainActivity.kt
@@ -1,26 +1,36 @@
 package com.talejalilov.yukatechexercise
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.*
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.android.gms.location.LocationServices
+import com.google.android.libraries.maps.model.LatLng
 import com.talejalilov.yukatechexercise.presentation.*
 import com.talejalilov.yukatechexercise.presentation.Authentication.AdminLoginScreen
 import com.talejalilov.yukatechexercise.presentation.Authentication.ChooseScreen
 import com.talejalilov.yukatechexercise.presentation.Authentication.SignUpScreen
 import com.talejalilov.yukatechexercise.presentation.Authentication.UserLoginScreen
+import com.talejalilov.yukatechexercise.presentation.viewmodel.MainActivityViewModel
 import com.talejalilov.yukatechexercise.ui.theme.YukaTechExerciseTheme
 import com.talejalilov.yukatechexercise.util.Screens
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainActivityViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             YukaTechExerciseTheme {
                     val navController = rememberNavController()
@@ -33,6 +43,49 @@ class MainActivity : ComponentActivity() {
 //                view.updatePadding(bottom = bottom)
 //                inserts
 //            }
+        }
+    }
+
+    private fun getLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this.applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            viewModel.permissionGrand(true)
+            getDeviceLocation()
+
+        }else{
+            Log.d("Exception", "Permission not granted")
+        }
+    }
+
+    private  fun getDeviceLocation(){
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        try {
+            if (viewModel.locationPermissionGranted.value ==true){
+                val locationResult = fusedLocationProviderClient.lastLocation
+
+                locationResult.addOnCompleteListener {
+                        task ->
+                    if (task.isSuccessful){
+                        val lastKnownLocation = task.result
+
+                        if (lastKnownLocation != null){
+                            viewModel.currentUserGeoCOord(
+                                LatLng(
+                                    lastKnownLocation.altitude,
+                                    lastKnownLocation.longitude
+                                )
+                            )
+                        }
+                    }else{
+                        Log.d("Exception"," Current User location is null")
+                    }
+                }
+
+            }
+
+        }catch (e: SecurityException){
+            Log.d("Exception", "Exception:  $e.message.toString()")
         }
     }
 }
